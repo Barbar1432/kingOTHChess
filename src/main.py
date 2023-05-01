4d1cd50fdb9886410e4d775fc006155b51286c22
Yetkin Zambelli <yetkin@surfer-172-29-4-177-hotspot.internet-for-guests.com>
2023-05-01 18:10:33 +0200

    some additions

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import pygame
-from board import board
+from board import board, get_piece
 
 #branch deneme 6
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 
 # Press the green button in the gutter to run the script.
@@ -21,7 +20,12 @@ clock = pygame.time.Clock()
 running = True
 sqSelected=()
 sec=[]
+new_cursor = None  # Clicked Piece
 while running:
+    if new_cursor is not None:
+        cursor_position = pygame.mouse.get_pos()
+        board.screen.blit(new_cursor, cursor_position)
+        pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,12 +35,17 @@ while running:
             row= row//board.square_size
             col=col//board.square_size
             sqSelected= (col,row)
-            sec.append(sqSelected)
-
+            if board.board[col][row] is not None:  # If the clicked tile is not empty:
+                print(board.board[col][row].name)
+                new_cursor = pygame.image.load('images/'+ board.board[col][row].name +'.png').convert_alpha()
+                new_cursor = pygame.transform.scale(new_cursor,(40,40))
+            if not (len(sec) == 0 and board.board[col][row] is None):  # can't move an empty tile
+                sec.append(sqSelected)
         if (len(sec)==2):
             board.move(sec[0],sec[1])
             board.draw()
             sec = []
+            new_cursor = None
     board.draw()
 
 
