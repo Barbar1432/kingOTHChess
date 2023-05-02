32ebb28670b6c98067b47d308217438ef8919208
Yetkin Zambelli <yetkin@surfer-172-29-4-177-hotspot.internet-for-guests.com>
2023-05-02 17:47:09 +0200

    Added 'Drag & Drop' mechanic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import pygame
-from board import board, get_piece
+from board import board
 
-#branch deneme 6
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -20,36 +19,43 @@ clock = pygame.time.Clock()
 running = True
 sqSelected=()
 sec=[]
-new_cursor = None  # Clicked Piece
+clicked_Piece_Image = None  # Clicked Piece
+clicked_Piece_Bool = False
+
 while running:
-    if new_cursor is not None:
-        cursor_position = pygame.mouse.get_pos()
-        board.screen.blit(new_cursor, cursor_position)
-        pygame.display.flip()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left Click is pushed
+            # print("Clicked")
             mouse_pos = pygame.mouse.get_pos()
-            row,col = mouse_pos
-            row= row//board.square_size
-            col=col//board.square_size
-            sqSelected= (col,row)
-            if board.board[col][row] is not None:  # If the clicked tile is not empty:
-                print(board.board[col][row].name)
-                new_cursor = pygame.image.load('images/'+ board.board[col][row].name +'.png').convert_alpha()
-                new_cursor = pygame.transform.scale(new_cursor,(40,40))
-            if not (len(sec) == 0 and board.board[col][row] is None):  # can't move an empty tile
+            row, col = mouse_pos
+            row = row//board.square_size
+            col = col//board.square_size
+            if len(sec) == 0 and board.board[col][row] is not None:  # If the clicked tile is not empty:
+                sqSelected = (col, row)
                 sec.append(sqSelected)
-        if (len(sec)==2):
-            board.move(sec[0],sec[1])
+                clicked_Piece_Bool = True
+                clicked_Piece_Image = pygame.image.load('images/' + board.board[col][row].name + '.png').convert_alpha()
+        if event.type == pygame.MOUSEBUTTONUP and len(sec) == 1:
+            # print("Stopped Clicking")
+            # len(sec) == 1 : If a piece was not already clicked, do nothing
+            row, col = pygame.mouse.get_pos()
+            dest_row = row//board.square_size
+            dest_col = col//board.square_size
+            sqSelected = (dest_col, dest_row)
+            sec.append(sqSelected)
+            clicked_Piece_Bool = False
+            clicked_Piece_Image = None
+        if len(sec) == 2:
+            board.move(sec[0], sec[1])
             board.draw()
             sec = []
-            new_cursor = None
+        if event.type == pygame.QUIT:
+            running = False
+    if clicked_Piece_Bool:
+        pos = pygame.mouse.get_pos()
+        board.screen.blit(clicked_Piece_Image,(pos[0],pos[1]))
+        pygame.display.flip()
     board.draw()
-
-
-
 
 
 
