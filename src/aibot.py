458abc361ad6caceaf946b95bfd22ba74a63e96f
yagizsworkspace99 <semerciogluyagiz@gmail.com>
2023-05-29 22:37:12 +0200

    “ygz”

--- src/aibot.py
+++ src/aibot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pygame
 from piece import Rook, Knight, Bishop, Queen, King, Pawn
 import random
@@ -16,49 +17,43 @@ class bot:
 
     def random_move(self, ai_bool, board):
         if ai_bool:  # AI-Bot is active
-            bewertung, path = alpha_beta(board, 3, float('-inf'), float('inf'), True)
+            bewertung, path = alpha_beta(board, 2, float('-inf'), float('inf'), True)
             move = path[0].lastmove
             print(move)
             sq, dest = move
             board.moveZa(sq, dest)
 
 def generate_mini_boards(board):
-    board_list = []
+
     list_of_possible_moves = []
     moves = board.legalMoves()
     for row in range(8):
         for col in range(8):
             piece = board.board[row][col]
-            if piece != None and board.Anzahlmoves % 2 == 0 and piece.color == 'white':
+            if piece is not None and ((board.Anzahlmoves % 2 == 0 and piece.color == 'white') or (board.Anzahlmoves % 2 == 1 and piece.color == 'black')):
                 legal_moves = moves[(row, col)]
-                # print("Piece in pos:", row, col, " has legal moves:", legal_moves)
                 while len(legal_moves) != 0:
                     move = legal_moves.pop()
                     possible_move = ((row, col), move)
                     list_of_possible_moves.append(possible_move)
-                    board_list.append(copy.deepcopy(board))
-            elif piece != None and board.Anzahlmoves % 2 == 1 and piece.color == 'black':
-                legal_moves = moves[(row, col)]
-                # print("Piece in pos:", row, col, " has legal moves:", legal_moves)
-                while len(legal_moves) != 0:
-                    move = legal_moves.pop()
-                    possible_move = ((row, col), move)
-                    list_of_possible_moves.append(possible_move)
-                    board_list.append(copy.deepcopy(board))
-    i = 0
-    for pos_moves in list_of_possible_moves:
-        sq, des = pos_moves
-        board_list[i].moveZa(sq, des)
-        i += 1
-    return board_list
+
+    return list_of_possible_moves
 
 def alpha_beta(node, depth, alpha, beta, is_max, path=[]):
     if depth == 0 or isTerminal(node):
+
+
         return node.bewertungsFunktion(), path
     if is_max:
         best_value = alpha
         best_path = None
-        for child in generate_mini_boards(node):
+        for move in generate_mini_boards(node):
+
+            child = copy_board(node)
+            do_move_for_ab(child,move)
+
+
+
             # print("At child:", child.boardInteger)
             value, child_path = alpha_beta(child, depth - 1, best_value, beta, False, path + [child])
             if value > best_value:
@@ -71,8 +66,11 @@ def alpha_beta(node, depth, alpha, beta, is_max, path=[]):
     else:
         best_value = beta
         best_path = None
-        for child in generate_mini_boards(node):
+        for move in generate_mini_boards(node):
             # print("At child:", child.boardInteger)
+            child = copy_board(node)
+            do_move_for_ab(child, move)
+
             value, child_path = alpha_beta(child, depth - 1, alpha, best_value, True, path + [child])
             if value < best_value:
                 best_value = value
@@ -86,4 +84,20 @@ def isTerminal(b_board):
     # TODO: If no more possible moves - Stalemate
     if b_board.finished:
         return True
+
+def copy_board(board):
+    new_board = board.__class__()
+
+    new_board.whiteKing = copy.deepcopy(board.whiteKing)
+    new_board.blackKing = copy.deepcopy(board.blackKing)
+    new_board.Anzahlmoves = board.Anzahlmoves
+    new_board.board = [[copy.deepcopy(piece) if piece is not None else None for piece in row] for row in board.board]
+    new_board.boardInteger = np.copy(board.boardInteger)
+
+    return new_board
+
+
+def do_move_for_ab(board, pos_moves):
+    sq, des = pos_moves
+    board.moveZa(sq, des)
 
